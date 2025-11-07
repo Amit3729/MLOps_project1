aba0b0f05f1d2bd84552d57da09412047b0219d5
Amit29 <pal98112016@gmail.com>
2025-11-07 21:25:49 +0545

    Project structure ready with env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Pal",
+    author_email="pal98112016@gmail.com",
+    packages=find_packages()
+
+)
